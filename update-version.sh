251ffe85da89487dc488567c722752e201a98095
tommieh <tommieh@uci.edu>
2023-01-25 16:32:44 -0800

    This is a commit

--- update-version.sh
+++ update-version.sh
@@ -6,16 +6,16 @@ git pull origin main
 
 # Save the current date and time in a file called version
 
-date > version
+date > /data/class/cogs106/tommieh/cogs106-tommie/version
 
 # Add the file to the repository
 
-git add version
+git add .
 
 #Commit the changes
 
-git commit -m "Update version"
+git commit -m "Updated version"
 
 # Push the changes to the remote repository
 
-git push origin main
+git push --set-upstream  origin main
